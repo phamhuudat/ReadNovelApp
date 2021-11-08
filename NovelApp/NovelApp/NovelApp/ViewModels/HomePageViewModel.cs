3d0d1c1b162ff70064661987e4677b9e75380745
Dat Pham Huu <Datph@fpts.com.vn>
2021-11-08 11:28:35 +0700

    Xử lý xong màn hình home

--- NovelApp/NovelApp/NovelApp/ViewModels/HomePageViewModel.cs
+++ NovelApp/NovelApp/NovelApp/ViewModels/HomePageViewModel.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using NovelApp.Models.BookGwModels;
 using NovelApp.Services.Book;
+using Prism.Commands;
 using Prism.Navigation;
 
 namespace NovelApp.ViewModels
@@ -12,10 +17,56 @@ namespace NovelApp.ViewModels
         public ObservableCollection<Novel> ListNovel { get => listNovel; set => SetProperty(ref listNovel, value); }
         private readonly IBookService _bookService;
         private ObservableCollection<Novel> listNovel;
-
+        public ICommand LoadMoreCommand { get; set; }
+        public ICommand SearchCommand { get; set; }
+        public ICommand ItemTappedCommand {get;set;}
+        private string _nameNovel;
         public HomePageViewModel(INavigationService navigationService, IBookService bookService) : base(navigationService)
         {
             _bookService = bookService;
+            LoadMoreCommand = new DelegateCommand<object>(LoadMore);
+            SearchCommand = new DelegateCommand<string>(SearchNovel);
+            ItemTappedCommand = new DelegateCommand<object>(ItemTapped);
+        }
+        private void ItemTapped(object obj)
+        {
+            var listView = obj as Syncfusion.ListView.XForms.SfListView;
+            if (listView == null) return;
+            var item = listView.SelectedItem;
+            listView.SelectedItem = null;
+
+        }
+        private async void SearchNovel(string name)
+        {
+            _nameNovel = name;
+            var list = await _bookService.SearchNovelList(name, 0);
+            if (list != null && list.Any())
+                ListNovel = new ObservableCollection<Novel>(list);
+        }
+        private async void LoadMore(object obj)
+        {
+            var listView = obj as Syncfusion.ListView.XForms.SfListView;
+            if (listView == null) return;
+            try
+            {
+                if (ListNovel == null || !ListNovel.Any())
+                    return;
+                listView.IsBusy = true;
+                await Task.Delay(1000);
+                var lastItem = ListNovel.Last();
+                IEnumerable<Novel> list = new List<Novel>();
+                if (string.IsNullOrEmpty(_nameNovel))
+                    list = await _bookService.GetNovelList(lastItem.ID);
+                else
+                    list = await _bookService.SearchNovelList(_nameNovel, lastItem.ID);
+                foreach (var item in list)
+                    ListNovel.Add(item);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.StackTrace);
+            }
+            listView.IsBusy = false;
         }
         public override async void OnNavigatedTo(INavigationParameters parameters)
         {
