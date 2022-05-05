a41a72191547e5611f36ca1047b21f44e7c34da0
phamhuudat <phamhuudatk58@gmail.com>
2022-05-06 06:36:19 +0700

    thêm tab library và user

---
+++ NovelApp/NovelApp/NovelApp/Views/BookSelf/DownloadView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace NovelApp.Views.BookSelf
+{
+    public partial class DownloadView : ContentView
+    {
+        public DownloadView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NovelApp/NovelApp/NovelApp/Views/BookSelf/RecentReadingView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace NovelApp.Views.BookSelf
+{
+    public partial class RecentReadingView : ContentView
+    {
+        public RecentReadingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
