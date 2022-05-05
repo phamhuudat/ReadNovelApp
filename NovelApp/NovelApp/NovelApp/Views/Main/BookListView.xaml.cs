a5a1fcaacbaa8c797b7b31abd6e8b56fe4dd04a1
phamhuudat <phamhuudatk58@gmail.com>
2022-05-05 07:36:45 +0700

    all

---
+++ NovelApp/NovelApp/NovelApp/Views/Main/BookListView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace NovelApp.Views.Main
+{
+    public partial class BookListView : ContentView
+    {
+        public BookListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NovelApp/NovelApp/NovelApp/Views/Main/BookShelfView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace NovelApp.Views.Main
+{
+    public partial class BookShelfView : ContentView
+    {
+        public BookShelfView()
+        {
+            InitializeComponent();
+        }
+    }
+}
