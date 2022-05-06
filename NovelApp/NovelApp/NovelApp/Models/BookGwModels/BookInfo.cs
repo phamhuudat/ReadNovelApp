6bca5a4c1a06d654b32102440492e46cba3c635f
Dat Pham Huu <Datph@fpts.com.vn>
2022-05-06 13:21:03 +0700

    add db book

---
+++ NovelApp/NovelApp/NovelApp/Models/BookGwModels/BookInfo.cs
@@ -0,0 +1,17 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NovelApp.Models.BookGwModels
+{
+    public class BookInfo : RealmObject
+    {
+        public int ID { get; set; }
+        public string BookName { get; set; }
+        public string Author { get; set; }
+        public int ReadState { get; set; }
+        public int ListType { get; set; }
+        public string LastReadTime { get; set; }
+    }
+}
---
+++ NovelApp/NovelApp/NovelApp/Services/DatabaseService/IDatabaseService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NovelApp.Services.DatabaseService
+{
+    public interface IDatabaseService
+    {
+        Task<bool> SaveBookInfo()
+    }
+}
